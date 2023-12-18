8a865ca20aa8f71019a55cdb0580c429df2d3a8d
Guillermo Esteban García <guillermo@MacBook-Pro-de-Guillermo.local>
2023-12-18 21:53:03 +0100

    Updated 2 first functions

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Sprint 3 IT Academy | Video management tool
 
-## Introduction
+#### Introduction
 
 A company in the audiovisual sector has asked us for a web application that will allow their employees to quickly find movies from a large database they have, since the process is currently done manually.
 
 You will be in charge of setting up the core of the application: all the logic of filtering and sorting of movies. You have 2 weeks to finish, which is how long this sprint lasts.
 
 <br>
+<br>
 
 ## Requirements
 
 
-1. Clone this repo
+1. Clone this repo!
 ```bash
 $ git clone https://github.com/IT-Academy-BCN/starter-code-frontend-sprint-3-movies
 ```
--- src/films.js
+++ src/films.js
@@ -1,14 +1,45 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+let result = array.map(movie => movie.director);
+console.log("EXERCISE 1 ->", result);
+return result;
 }
 
-// Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
+// Function to show directors on the screen
+function showDirectors() {
+// Get the array of directors
+const directors = getAllDirectors(movies);
+
+// Display directors in the container
+const directorsContainer = document.getElementById('directorsContainer');
+directorsContainer.innerHTML = "<h2>Directors:</h2><p>" + directors.join(', ') + "</p>";
 }
+
+  // Exercise 2: Get the array of movies for a specific director.
+  function getMoviesFromDirector(moviesArray, director) {
+    return moviesArray.filter(movie => movie.director === director);
+  }
+
+  // Function to show movies for a specific director on the screen
+  function showMoviesForDirector() {
+    // Get the director input from the user
+    const directorInput = document.getElementById('directorInput').value;
+
+    // Get the array of movies for the specified director
+    const moviesForDirector = getMoviesFromDirector(movies, directorInput);
+
+    // Display movies for the specific director in the container
+    const moviesForDirectorContainer = document.getElementById('moviesForDirectorContainer');
+    
+    moviesForDirectorContainer.innerHTML = "<h2>Movies for Director:</h2>";
+
+    if (moviesForDirector.length > 0) {
+      const moviesList = moviesForDirector.map(movie => movie.title).join(', ');
+      moviesForDirectorContainer.innerHTML += `<p><strong>${directorInput}</strong>: ${moviesList}</p>`;
+    } else {
+      moviesForDirectorContainer.innerHTML += `<p>No movies found for <strong>${directorInput}</strong></p>`;
+    }
+  }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
